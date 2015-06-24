4e920f57a5c95787fd8312180e0ebc255c610f0f
compurat <pieter.roskam@gmail.com>
2015-06-24 20:11:37 +0200

    [1] reworked the code. For spring environment I need to write a class that reads the beans. to much. Did it the old fashioned way.

--- src/main/java/com/purat/client/ServiceClient.java
+++ src/main/java/com/purat/client/ServiceClient.java
@@ -1,30 +1,27 @@
 package com.purat.client;
 
+import com.purat.MultipartBuilder;
 import com.purat.ServiceConnector;
-import com.purat.client.annotation.ConnectionName;
-import com.purat.client.annotation.FieldName;
-import com.purat.constants.FieldNames;
+import com.purat.ServiceConnectorImpl;
 import org.apache.http.entity.mime.content.AbstractContentBody;
-import org.apache.http.entity.mime.content.StringBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.UnsupportedEncodingException;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Created by compurat on 21-6-15.
  */
-@Component
+
 public class ServiceClient<T> {
 
-    @Autowired
-    private ServiceConnector serviceConnector;
 
-    public String connect(String url, Map<String, AbstractContentBody> parts) {
+
+
+    public String connect(String url, T bean) {
+        ServiceConnector serviceConnector = new ServiceConnectorImpl();
+        MultipartBuilder<T> multipartBuilder = new MultipartBuilder<T>();
+        Map<String, AbstractContentBody> parts = multipartBuilder.createParts(bean);
         return serviceConnector.getServiceResponse(url, parts);
     }
 
